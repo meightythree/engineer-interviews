9025b32a6bcb821aa66728739b7fa3e4269369ba
meightythree <palkollar1995@gmail.com>
2024-04-04 09:49:44 +0200

    feat: :package: install daisyui

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -3,26 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  template: `
-    <div>
-      <a href="https://analogjs.org/" target="_blank">
-        <img alt="Analog Logo" class="logo analog" src="/analog.svg" />
-      </a>
-    </div>
-
-    <h2>Analog</h2>
-
-    <h3>The fullstack meta-framework for Angular!</h3>
-
-    <div class="card">
-      <button type="button" (click)="increment()">Count {{ count }}</button>
-    </div>
-
-    <p class="read-the-docs">
-      For guides on how to customize this project, visit the
-      <a href="https://analogjs.org" target="_blank">Analog documentation</a>
-    </p>
-  `,
+  template: ` <button class="btn">asd</button> `,
   styles: [
     `
       .logo {
